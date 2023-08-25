653a44817dca5e3da717d9d1f6f77ac259085d98
Raju Lamsal <rajulamsal62@gmail.com>
2023-08-25 18:47:19 +1000

    adding chakraui to the frontend and created header, slider, footer component

---
+++ frontend/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import Navbar from "./Navbar/Navbar";
+import Slider from "./Slider/Slider";
+
+const Header = () => {
+  return (
+    <div>
+      <Navbar />
+      <Slider />
+    </div>
+  );
+};
+
+export default Header;
